41a7855cbb5cca7b07d0f12972dd72648bfa50da
mykle jones <mykle.j91@gmail.com>
2019-09-04 10:42:01 -0400

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
+    attr_accessor
+    attr_reader :customer, :restaurant, :review, :rating
+    @@all = []
+    def   initialize( customer,restaurant, review ,rating)
+        @customer = customer
+        @restaurant = restaurant
+        @review = review
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        return @@all
+       end
+       
 end
 
