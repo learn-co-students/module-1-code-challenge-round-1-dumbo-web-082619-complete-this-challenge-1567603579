6e867958bc9ea2a8ccc1619c4446274ed7207874
NMontesino <namontesino@gmail.com>
2019-09-04 10:40:29 -0400

    Done

--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+nick = Customer.new("Nick", "Montesino")
+nick2 = Customer.new("Nick", "Martella")
+cy = Customer.new("Cydel", "Giraudel")
+ro = Customer.new("Rohan", "Mukherjee")
+
+moes = Restaurant.new("Moe's")
+stuff = Restaurant.new("Stuff yer Face")
+
+nick.add_review(moes, "This place is literal trash", 2)
+nick.add_review(moes, "I was being harsh", 4)
+cy.add_review(moes, "It's okay", 3)
+
+found = Restaurant.find_by_name("Stuff yer Face")
+
+puts found.name
+
+# binding.pry
+# 0 #leave this here to ensure binding.pry isn't the last line
