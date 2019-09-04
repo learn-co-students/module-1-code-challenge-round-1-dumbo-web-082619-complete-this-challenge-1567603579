63031071352b5dc9ef1b10553d820d3c9538e5e1
github username <github email address>
2019-09-04 10:35:21 -0400

    First commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,40 @@
 class Customer
   attr_reader :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
-  def full_name
+  # Returns all customers
+  def self.all()
+    @@all
+  end
+
+  def full_name()
     "#{first_name} #{last_name}"
   end
+
+  # Creates a new review for user
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  # Returns reviews per user
+  def user_reviews()
+    Review.all().select {|review| review.customer == self}
+  end
+
+  # Returns n# of reviews per user
+  def num_reviews()
+    self.user_reviews().count
+  end
+
+  # Returns restaunts per user
+  def restaurants()
+    self.user_reviews().map {|review| review.restaurant}.uniq
+  end
+
 end
