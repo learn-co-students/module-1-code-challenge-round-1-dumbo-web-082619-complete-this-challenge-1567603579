76083e3de3678abb43b225a11fc3e09c051d7040
Mark DeNardo <mark.denardo@gmail.com>
2019-09-04 10:41:15 -0400

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
-  
+    attr_reader :customer, :restaurant, :review
+    
+    @@all = []
+
+    def initialize
+        @customer=customer
+        @restaurant=restaurant
+        @review = review
+        @@all << all
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
 end
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,15 @@
 require 'bundler/setup'
+
 Bundler.require
 require_rel '../app'
+
+c1 = Customer.new("Bobby", "Birdman")
+c2 = Customer.new("Bernice", "Bubbles")
+c3 = Customer.new("Best", "Blueberry")
+
+r1 = Restaurant.new("Charlie's Steakhouse")
+r2 = Restaurant.new("Cocina")
+r3 = Restaurant.new("Catcher in the Rye")
+
+rv1 = 
+
